f31bcab28c23ffa0e6a7a87666fc120ff6722e15
Mikely Barboza <barbozamikely@gmail.com>
2023-02-09 05:00:02 -0300

    Inicio em JS

---
+++ inicio/comentario.js
@@ -0,0 +1,14 @@
+// console.log é usado para exibir algo na tela do computaor!
+console.log('Passo #03');
+console.log('Passo #01'); // mais um exemplo de console.log
+//console.log('Passo #02');
+
+/**
+ * esse é
+ * um 
+ * comentário
+ * de múltiplas
+ * linhas
+ */
+
+console.log('passo #02');
